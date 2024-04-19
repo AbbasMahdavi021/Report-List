d075441b56dcc712750c41df2767090ed8d26812
AbbasMahdavi021 <abbas.mahdavi021@gmail.com>
2024-04-19 04:42:54 -0700

    Add dummy data, map data in modal-content Reports.jsx, display name,date,download logo

--- src/components/reports/Reports.jsx
+++ src/components/reports/Reports.jsx
@@ -1,3 +1,6 @@
+import dummyData from "@/constants/dummyReports";
+import { GrDocumentDownload } from "react-icons/gr";
+
 const Reports = () => {
   return (
     <div className="w-full">
@@ -7,7 +10,20 @@ const Reports = () => {
         <div>Download</div>
       </div>
 
-      <div className="flex flex-col"></div>
+      <div className="flex flex-col">
+        {dummyData.map((report, index) => (
+          <div
+            key={index}
+            className="flex flex-row text-sm p-2 border-b border-gray-200"
+          >
+            <div className="ml-4 w-24">{report.datetime}</div>
+            <div className="w-3/4">{report.reportName}</div>
+            <div className="flex justify-center w-16 text-3xl text-center">
+              <GrDocumentDownload />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/constants/dummyReports.js
@@ -0,0 +1,52 @@
+const dummyData = [
+  {
+    datetime: "2021-04-22T16:02:00",
+    reportName: "APP_URL_USAGE(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+  },
+  {
+    datetime: "2021-07-20T00:00:00",
+    reportName: "RUR_L1_01_01_2021_01_03_2021.csv",
+  },
+  {
+    datetime: "2021-07-19T00:00:00",
+    reportName: "RUR_L2_(Exclude_NonWorking_Days)_4_1_2021_4_1_2021.csv",
+  },
+  {
+    datetime: "2021-07-19T00:00:00",
+    reportName: "RUR_L2_(Exclude_NonWorking_Days)_4_1_2021_4_1_2021.csv",
+  },
+  {
+    datetime: "2021-07-19T00:00:00",
+    reportName: "RUR_L2_(Exclude_NonWorking_Days)_4_1_2021_4_1_2021.csv",
+  },
+  {
+    datetime: "2021-07-19T00:00:00",
+    reportName: "RUR_L2_(Exclude_NonWorking_Days)_4_1_2021_4_1_2021.csv",
+  },
+  {
+    datetime: "2021-07-19T00:00:00",
+    reportName: "RUR_L2_(Exclude_NonWorking_Days)_4_1_2021_4_1_2021.csv",
+  },
+  {
+    datetime: "2021-07-19T00:00:00",
+    reportName: "RUR_L2_(Exclude_NonWorking_Days)_4_1_2021_4_1_2021.csv",
+  },
+  {
+    datetime: "2021-07-19T00:00:00",
+    reportName: "RUR_L2_(Exclude_NonWorking_Days)_4_1_2021_4_1_2021.csv",
+  },
+  {
+    datetime: "2021-07-19T00:00:00",
+    reportName: "RUR_L2_(Exclude_NonWorking_Days)_4_1_2021_4_1_2021.csv",
+  },
+  {
+    datetime: "2021-07-19T00:00:00",
+    reportName: "RUR_L2_(Exclude_NonWorking_Days)_4_1_2021_4_1_2021.csv",
+  },
+  {
+    datetime: "2021-07-19T00:00:00",
+    reportName: "RUR_L2_(Exclude_NonWorking_Days)_4_1_2021_4_1_2021.csv",
+  },
+];
+
+export default dummyData;
