e8a5351eb2c1ce2a8117295921c91787845fd122
AbbasMahdavi021 <abbas.mahdavi021@gmail.com>
2024-04-20 03:50:36 -0700

    Implement download simulation, downloads file with correct name/exe with filler data

--- src/components/reports/ReportCard.jsx
+++ src/components/reports/ReportCard.jsx
@@ -1,6 +1,27 @@
-import { TbFileDownload } from "react-icons/tb";
-
 const ReportCard = ({ date, reportName, index }) => {
+  /* For demonstration purposes, handleDownload 
+    simulates a download action. In production, 
+    it would fetch files from an S3 bucket or server. */
+  const handleDownload = () => {
+    // Create a dummy data blob
+    const dummyData = `The Content of ${reportName}`;
+    const blob = new Blob([dummyData], { type: "text/plain" });
+
+    // download url and link
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = reportName; //Report name has extension
+    document.body.appendChild(link);
+
+    //Download
+    link.click();
+
+    // Clean up
+    URL.revokeObjectURL(url);
+    document.body.removeChild(link);
+  };
+
   const formattedDate = new Date(date).toLocaleDateString("en-US", {
     month: "2-digit",
     day: "2-digit",
@@ -23,8 +44,13 @@ const ReportCard = ({ date, reportName, index }) => {
       <div className=" w-3/4 overflow-hidden">
         <div className="mx-1  truncate">{reportName}</div>
       </div>
-      <div className="flex justify-center w-24 mx-1 text-2xl">
-        <img className="w-9" src="/document-download.svg" alt="download" />
+      <div className="flex justify-center w-16 mx-1 text-2xl">
+        <img
+          onClick={handleDownload}
+          className="w-9 cursor-pointer"
+          src="/document-download.svg"
+          alt="download"
+        />
       </div>
       <div className="flex justify-start"></div>
     </div>
